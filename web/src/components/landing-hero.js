9cde2a971393f5ec68ecfe0b3b8deaf40d8cc514
lucas.homer <lucas.homer@selectquote.com>
2020-05-06 22:15:43 -0700

    import image asset into landing page

--- web/src/components/landing-hero.js
+++ web/src/components/landing-hero.js
@@ -1,12 +1,24 @@
 import React from 'react'
+import { buildImageObj } from '../lib/helpers'
+import { imageUrlFor } from '../lib/image-url'
 import styles from './landing-hero.module.css'
 
-export default function LandingHero () {
+export default function LandingHero ({ heroImage, description }) {
   return (
     <section className={styles.root}>
       <div className={styles.hero}>
-        <h3>Hi! I'm Pat. Welcome to my site.</h3>
-        <img src='undraw_basketball.svg' />
+        <h3 className={styles.description}>{description}</h3>
+        {heroImage && heroImage.asset && (
+          <img
+            className={styles.heroImage}
+            src={imageUrlFor(buildImageObj(heroImage))
+              .width(200)
+              .height(Math.floor((9 / 16) * 200))
+              .fit('crop')
+              .url()}
+            alt={heroImage.alt}
+          />
+        )}
       </div>
     </section>
   )
